d2fa3b55d03b7e00e146b0731957ffa4a7664b66
Saulius Lukauskas <saulius.lukauskas@helmholtz-muenchen.de>
2020-10-28 10:48:58 +0100

    Track views of help bubble

--- loadbalancer/react-frontend/src/components/common/HelpQuestionMark.js
+++ loadbalancer/react-frontend/src/components/common/HelpQuestionMark.js
@@ -5,29 +5,40 @@ import Popover from 'react-bootstrap/Popover';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
+import { useMatomo } from '@datapunt/matomo-tracker-react';
 
-export default class HelpQuestionMark extends React.PureComponent {
-    render() {
-        const { title, children } = this.props;
-        const popover = (
-            <Popover id="popover-basic">
-                <Popover.Title as="h3">{title}</Popover.Title>
-                <Popover.Content>
-                    {children}
-                </Popover.Content>
-            </Popover>
-        );
 
-        return (
-            <OverlayTrigger trigger={['hover', 'focus']} placement="auto" overlay={popover}>
-                <Button as="a" variant="link" size="sm" className="pb-0 pt-0">
-                    <FontAwesomeIcon icon={faQuestionCircle} />
-                </Button>
-            </OverlayTrigger>
-        );
-    }
+export default function HelpQuestionMark(props) {
+    const { title, children } = props;
+    const { trackEvent } = useMatomo();
+
+    const handleToggle = (show) => {
+        if (show) {
+            trackEvent({
+                category: 'help-text',
+                action: 'open-help-bubble',
+                name: title,
+            });
+        }
+    };
+
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Title as="h3">{title}</Popover.Title>
+            <Popover.Content>
+                {children}
+            </Popover.Content>
+        </Popover>
+    );
+
+    return (
+        <OverlayTrigger trigger={['hover', 'focus']} placement="auto" onToggle={handleToggle} overlay={popover}>
+            <Button as="a" variant="link" size="sm" className="pb-0 pt-0">
+                <FontAwesomeIcon icon={faQuestionCircle} />
+            </Button>
+        </OverlayTrigger>
+    );
 }
-
 
 HelpQuestionMark.propTypes = {
     // eslint-disable-next-line react/no-unused-prop-types
