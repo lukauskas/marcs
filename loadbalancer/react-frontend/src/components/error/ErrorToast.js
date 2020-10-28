02d979e15f4dbbf181ac05e631868f4947d4eb79
Saulius Lukauskas <saulius.lukauskas@helmholtz-muenchen.de>
2020-10-28 11:09:38 +0100

    Track errors

--- loadbalancer/react-frontend/src/components/error/ErrorToast.js
+++ loadbalancer/react-frontend/src/components/error/ErrorToast.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Toast from 'react-bootstrap/Toast';
 import PropTypes from 'prop-types';
 import Badge from 'react-bootstrap/Badge';
 import './styles/ErrorToast.css';
+import {useMatomo} from "@datapunt/matomo-tracker-react";
 
-export default class ErrorToast extends React.PureComponent {
-    render() {
-        const { onClose, title, errorMessage, badgeVariant, errorType } = this.props;
+export default function ErrorToast(props) {
 
-        return (
-            <div
-                // aria-live="polite"
-                //  aria-atomic="true"
-                className="d-flex justify-content-center align-items-center error-toast-container"
-            >
+    const { onClose, title, errorMessage, badgeVariant, errorType } = props;
+    const { trackEvent } = useMatomo();
 
-                <Toast role="alert" onClose={onClose}>
-                    <Toast.Header>
-                        <Badge variant={badgeVariant} className="mr-1">{errorType}</Badge>
-                        <span style={{ color: 'black' }}>{title}</span>
-                    </Toast.Header>
-                    <Toast.Body>
-                        <p>{errorMessage}</p>
-                        <p>Try refreshing the page. If the problem persists, contact the administrator.</p>
-                    </Toast.Body>
-                </Toast>
-            </div>
-        );
-    }
+    useEffect(() => {
+        trackEvent({
+            category: 'error',
+            action: 'error-toast-shown',
+            name: errorType,
+            value: errorMessage,
+        });
+    }, [errorMessage, errorType]);
+
+    return (
+        <div
+            // aria-live="polite"
+            //  aria-atomic="true"
+            className="d-flex justify-content-center align-items-center error-toast-container"
+        >
+
+            <Toast role="alert" onClose={onClose}>
+                <Toast.Header>
+                    <Badge variant={badgeVariant} className="mr-1">{errorType}</Badge>
+                    <span style={{ color: 'black' }}>{title}</span>
+                </Toast.Header>
+                <Toast.Body>
+                    <p>{errorMessage}</p>
+                    <p>Try refreshing the page. If the problem persists, contact the administrator.</p>
+                </Toast.Body>
+            </Toast>
+        </div>
+    );
 }
 
 
